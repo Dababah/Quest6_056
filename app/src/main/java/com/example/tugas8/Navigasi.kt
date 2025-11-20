45e6888c9df3a8d92bee1401e6ff9b205e33600d
Dababah <pawasalee96@gamil.com>
2025-11-20 13:35:22 +0700

    nambahin parameter viewmodel

--- app/src/main/java/com/example/tugas8/Navigasi.kt
+++ app/src/main/java/com/example/tugas8/Navigasi.kt
@@ -1,57 +1,9 @@
-package com.example.tugas8
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.example.tugas8.viewmodel.SiswaViewModel
-
-
-enum class Navigasi {
-    Formulirku,
-
-    Detail
-}
 
 @Composable
-fun DataApp(
+fun Datasiswa(
     navController : NavHostController = rememberNavController(),
     modifier: Modifier
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController
-){
-    Scaffold { isiRuang ->
-        NavHost(
-            navController = navController,
-            startDestination = Navigasi.Formulirku.name,
-            modifier = Modifier.padding(paddingValues = isiRuang)
-        ) {
-            composable(route = Navigasi.Formulirku.name) {
-                FormIsian(
-                    // pilihanJK = JenisK.map { id -> resources.getString(id) }
-                    OnSubmitBtnClick = {
-                        navController.navigate(route = Navigasi.Detail.name)
-                    }
-                )
-            }
-            composable(route = Navigasi.Detail.name){
-                TampilData(
-                    onBackBtnClick = {
-                        cancelAndBackToFormulirku(navController)
-                    }
-                )
-            }
-        }
-    }
-}
-
-private fun  cancelAndBackToFormulirku(
-    navController: NavHostController
-){
-    navController.popBackStack(Navigasi.Formulirku,
-        inclusive = false)
-}
+)
